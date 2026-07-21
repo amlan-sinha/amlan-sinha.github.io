cc269d797ab3daf32e829dddd01dd28dd3f559cf
Amlan Sinha <amlans@princeton.edu>
2026-07-21 18:30:56 -0400

    Update personal information

--- assets/js/typing.js
+++ assets/js/typing.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     var typed = new Typed("#typed-text", {
-        strings: ["Aerospace Engineer", "Problem Solver", "Photographer"],
+        strings: ["Quantitative Researcher", "Aerospace Engineer", "Photographer"],
         typeSpeed: 100,
         backSpeed: 50,
         backDelay: 1500,
